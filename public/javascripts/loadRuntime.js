db78760af6d601ddd7df2417c42ce63d4afc2e37
TOUZE Alexandre <alexandre.touze@orange.com>
2016-11-17 18:17:36 +0100

    Work in Progress

--- public/javascripts/loadRuntime.js
+++ public/javascripts/loadRuntime.js
@@ -9,8 +9,9 @@ var hypertyURIC = (domain, hyperty) => `hyperty-catalogue://catalogue.${domain}/
 
 /** Load Runtme reThink **/
 function loadreThink() {
+    RUNTIME = runtimeURL = domain = hypertyConnector = null;
     //Rethink runtime is included in index.html
-    registerDomain()
+    registerDomain();
 }
 
 function registerDomain() {
@@ -46,22 +47,96 @@ function loadHypertyConnector() {
         result.instance.onInvitation(function (controller, identity) {
             notificationHandler(controller, identity);
         });
+    }).catch(function (err) {
+
+        console.log("err");
+
     });
 }
 
 
 function notificationHandler(controller, identity) {
+
     console.log('controller', controller);
     console.log('identity', identity);
+    $('#inComingCall').modal();
 }
 
 function callUser() {
     var thisEmail = $(this).attr('rel');
-    hypertyConnector.instance.search.users([thisEmail], [domain], ['audio', 'video']).then(emailDiscovered).catch(emailDiscoveredError);
-    //console.log('mail', thisEmail);
+    hypertyConnector.instance.search.users([thisEmail], [domain], ['connection'], ['audio', 'video']).then(emailDiscovered).catch(emailDiscoveredError);
 }
 
 function emailDiscovered(result) {
-
-   console.log('result', result);
+    result.forEach((hyperty) => {
+        var userURL = hyperty.userID;
+        var hypertyURL = hyperty.hypertyID;
+        var domain = hypertyURL.substring(hypertyURL.lastIndexOf(':') + 3, hypertyURL.lastIndexOf('/'));
+        openVideo(userURL, domain);
+    });
 }
+
+function emailDiscoveredError(err) {
+
+    console.log('err', err);
+}
+
+function openVideo(userUrl, userdomain) {
+    var options = { video: true, audio: true };
+    getUserMedia(options).then(function (mediaStream) {
+
+    }).then(function (controller) {
+        showVideo(controller)
+        $('.my-video')[0].src = URL.createObjectURL(mediaStream);
+    }).catch(function (reason) {
+        console.log(reason);
+    })
+
+
+}
+
+/**
+* Video management
+*/
+function getUserMedia(constraints) {
+    return new Promise(function (resolve, reject) {
+        navigator.mediaDevices.getUserMedia(constraints)
+            .then(function (mediaStream) {
+                resolve(mediaStream);
+            })
+            .catch(function (reason) {
+                reject(reason);
+            });
+    });
+}
+
+function showVideo() {
+    controller.onAddStream(function (event) {
+        processVideo(event);
+    });
+
+    controller.onDisconnect(function (identity) {
+        hideVideo();
+        disconnecting();
+    });
+
+    $('.videoSection .camera.btn').on('click', function (event) {
+        event.preventDefault();
+        controller.disableVideo().then(function (status) {
+            console.log(status, 'camera');
+            var icon = 'videocam_off';
+            var text = 'Disable Camera';
+            if (!status) {
+                text = 'Enable Camera';
+                icon = 'videocam';
+            }
+
+            var iconEl = '<i class="material-icons left">' + icon + '</i>';
+            $(event.currentTarget).html(iconEl);
+        }).catch(function (e) {
+            console.error(e);
+        });
+
+    });
+}
+
